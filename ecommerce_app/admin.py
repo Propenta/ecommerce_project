12c3699c930f99fb9fa84bff9099cf7138f794b1
Noubissie237 <wilfried.noubissie@facsciences-uy1.cm>
2025-03-24 00:34:12 +0100

    Extraction de données

--- ecommerce_app/admin.py
+++ ecommerce_app/admin.py
@@ -6,4 +6,10 @@ class AdminArticle(admin.ModelAdmin):
     list_display = ('nom', 'description', 'prix', 'quantite_stock', 'image')
     search_fields = ('nom', 'prix')
 
+class UtilisateurAdmin(admin.ModelAdmin):
+    list_display = ('telephone', 'last_name', 'first_name', 'email',)
+    search_fields = ('username', 'email',)
+
+
 admin.site.register(Article, AdminArticle)
+admin.site.register(Utilisateur, UtilisateurAdmin)
--- ecommerce_app/models.py
+++ ecommerce_app/models.py
@@ -2,6 +2,14 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class Utilisateur(AbstractUser):
+    telephone = models.CharField(max_length=15, blank=True, null=True, verbose_name="Téléphone")
+    adresse = models.CharField(max_length=255, blank=True, null=True, verbose_name="Adresse")
+
+    def __str__(self):
+        return self.username
+    
+    
 class Article(models.Model):
     nom = models.CharField(max_length=50)
     description = models.TextField()
@@ -12,9 +20,3 @@ class Article(models.Model):
     def __str__(self):
         return f"{self.nom} - {self.prix} XAF"
     
-class Utilisateur(AbstractUser):
-    telephone = models.CharField(max_length=15, blank=True, null=True, verbose_name="Téléphone")
-    adresse = models.CharField(max_length=255, blank=True, null=True, verbose_name="Adresse")
-
-    def __str__(self):
-        return self.username
