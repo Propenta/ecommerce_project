7b323cf96f2f1b5abfcd2d0b3fca63f54e9df073
Noubissie237 <wilfried.noubissie@facsciences-uy1.cm>
2025-03-23 22:55:38 +0100

    Intégration des images

--- ecommerce_app/admin.py
+++ ecommerce_app/admin.py
@@ -3,7 +3,7 @@ from .models import *
 
 # Register your models here.
 class AdminArticle(admin.ModelAdmin):
-    list_display = ('nom', 'description', 'prix', 'quantite_stock')
+    list_display = ('nom', 'description', 'prix', 'quantite_stock', 'image')
     search_fields = ('nom', 'prix')
 
 admin.site.register(Article, AdminArticle)
--- ecommerce_app/models.py
+++ ecommerce_app/models.py
@@ -6,6 +6,7 @@ class Article(models.Model):
     description = models.TextField()
     quantite_stock = models.IntegerField(("Quantité en stock"))
     prix = models.FloatField(("Prix (FCFA)"))
+    image = models.ImageField(upload_to='articles/', blank=True, null=True, verbose_name="Image de l'article")
 
     def __str__(self):
         return f"{self.nom} - {self.prix} XAF"
